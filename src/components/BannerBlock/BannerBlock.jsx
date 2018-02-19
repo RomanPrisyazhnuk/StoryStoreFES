8a41b3d16a896204aec30c3c71173a0f2d4dda32
romanpr@123software.ru <Emissare>
2018-02-19 18:50:28 +0200

    CH-prepare main page

--- src/components/BannerBlock/BannerBlock.jsx
+++ src/components/BannerBlock/BannerBlock.jsx
@@ -1,14 +1,15 @@
 import './BannerBlock.scss';
+import {bannerItemFirst, bannerItemSecond, bannerItemThird, bannerItemFourth} from 'constants/bannersDataConstants';
 import BannerCarousel from 'components/BannerBlock/BannerCarousel';
 import React from 'react';
 
 const BannerBlock = () => {
     return (
         <div className="st-banner-block" >
-            <BannerCarousel style={{id: 1, top: 100, left: 50, width: 450, height: 250}}/>
-            <BannerCarousel style={{id: 2, bottom: 65, left: 50, width: 450, height: 250}}/>
-            <BannerCarousel style={{id: 3, top: 100, right: 50, width: 450, height: 250}}/>
-            <BannerCarousel style={{id: 4, bottom: 65, right: 50, width: 450, height: 250}}/>
+            <BannerCarousel style={{id: 1, top: 100, left: 50, width: 450, height: 250}} items={bannerItemFirst}/>
+            <BannerCarousel style={{id: 2, bottom: 65, left: 50, width: 450, height: 250}} items={bannerItemSecond}/>
+            <BannerCarousel style={{id: 3, top: 100, right: 50, width: 450, height: 250}} items={bannerItemThird}/>
+            <BannerCarousel style={{id: 4, bottom: 65, right: 50, width: 450, height: 250}} items={bannerItemFourth}/>
         </div>
     );
 };
--- src/components/BannerBlock/BannerCarousel/BannerCarousel.jsx
+++ src/components/BannerBlock/BannerCarousel/BannerCarousel.jsx
@@ -2,27 +2,6 @@ import './BannerCarousel.scss';
 import {Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption} from 'reactstrap';
 import React, { Component } from 'react';
 
-const items = [
-    {
-        id: 1,
-        src: 'http://mettermedia.com/wp-content/uploads/group-video-calling-full-screen.png',
-        altText: 'Play online',
-        caption: 'Slide 1'
-    },
-    {
-        id: 2,
-        src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-        altText: 'Create unique scenario',
-        caption: 'Slide 2'
-    },
-    {
-        id: 3,
-        src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-        altText: 'Store your game information here',
-        caption: 'Slide 3'
-    }
-];
-
 class BannerCarousel extends Component {
     constructor (props) {
         super(props);
@@ -44,13 +23,13 @@ class BannerCarousel extends Component {
 
     next () {
         if (this.animating) return;
-        const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+        const nextIndex = this.state.activeIndex === this.props.items.length - 1 ? 0 : this.state.activeIndex + 1;
         this.setState({ activeIndex: nextIndex });
     }
 
     previous () {
         if (this.animating) return;
-        const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+        const nextIndex = this.state.activeIndex === 0 ? this.props.items.length - 1 : this.state.activeIndex - 1;
         this.setState({ activeIndex: nextIndex });
     }
 
@@ -62,16 +41,16 @@ class BannerCarousel extends Component {
     render () {
         const { activeIndex } = this.state;
 
-        const slides = items.map((item) => {
+        const slides = this.props.items.map((item) => {
             return (
                 <CarouselItem
-                    className="custom-tag"
+                    className={`custom-tag custom-tag-${item.id}-${this.props.style.id}`}
                     tag="div"
                     key={item.id}
                     onExiting={this.onExiting}
                     onExited={this.onExited}
                 >
-                    <CarouselCaption className="text-danger" captionText={item.caption} captionHeader={item.caption} />
+                    <CarouselCaption className="text-danger" captionText={item.caption} captionHeader={item.altText} />
                 </CarouselItem>
             );
         });
@@ -84,14 +63,20 @@ class BannerCarousel extends Component {
                             position: absolute;
                             ${this.props.style.top ? 'top:' + this.props.style.top : 'bottom:' + this.props.style.bottom}px;
                             ${this.props.style.left ? 'left:' + this.props.style.left : 'right:' + this.props.style.right}px;
+
                             width: ${this.props.style.width}px;
                             height: ${this.props.style.height}px;
                         }
-                        .custom-tag {
-                max-width: 100%;
-                height: 350px;
-                background: black;
-              }`
+                        .custom-tag-1-${this.props.style.id}{
+                            background-image: url(${this.props.items[0].src});
+                        }
+                        .custom-tag-2-${this.props.style.id}{
+                            background-image: url(${this.props.items[1].src});
+                        }
+                        .custom-tag-3-${this.props.style.id}{
+                            background-image: url(${this.props.items[2].src});
+                        }
+                      `
                     }
                 </style>
                 <div className={`ch-banner-carousel-${this.props.style.id}`}>
@@ -100,7 +85,7 @@ class BannerCarousel extends Component {
                         next={this.next}
                         previous={this.previous}
                     >
-                        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+                        <CarouselIndicators items={this.props.items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
                         {slides}
                         <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
                         <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
